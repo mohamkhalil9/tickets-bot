abc60aa889018bfe4c871974c9fc47a091b1ac1e
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-05-25 22:02:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -103,7 +103,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`!new to make ticket | #♛ ExPlosive ♛`,"http://twitch.tv/Death Shop")
+client.user.setGame(`-new to make ticket | #♛ Fantasy ♛`,"http://twitch.tv/Death Shop")
 client.user.setStatus("online")
 });
 
